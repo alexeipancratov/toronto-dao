a57bdc8542c8f8dacfcc21b97b1850d7b0d591a3
Alexei Pancratov <drivergear@gmail.com>
2023-06-24 17:59:26 -0400

    Added event descriptions

--- ui/src/task-verification/TaskVerification.tsx
+++ ui/src/task-verification/TaskVerification.tsx
@@ -6,6 +6,7 @@ import contractAbi from "../abis/abi.json";
 
 function TaskVerification() {
   const [ethAddress, setEthAddress] = useState("");
+  const [task, setTask] = useState("1"); // [1, 2, 3, 4]
   const { ethereum } = useMetaMask();
 
   const sendTransaction = async () => {
@@ -31,33 +32,59 @@ function TaskVerification() {
     setEthAddress(e.target.value);
   }
 
+  const onTaskChange = (e: any) => {
+    setTask(e.target.value);
+  }
+
   return (
-    <Form onSubmit={onFormSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>ETH Address</Form.Label>
-        <Form.Control type="text" placeholder="0x000000000000000000" value={ethAddress} onChange={onAddressChange} />
-      </Form.Group>
-
-      <div className='text-center qr-code'>
-        <img src="./qr-code.png" className="img-fluid" alt="..." width="100"></img>
-      </div>
-
-      <section className='main-section'>
-        <Form.Group>
-          <Form.Label>Task</Form.Label>
-          <Form.Select>
-            <option value="1">Member registration</option>
-            <option value="2">Event attendance</option>
-          </Form.Select>
+    <>
+      <h1 className='text-left'>Verify Task</h1>
+      <Form onSubmit={onFormSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>ETH Address</Form.Label>
+          <Form.Control type="text" placeholder="0x000000000000000000" value={ethAddress} onChange={onAddressChange} />
         </Form.Group>
 
-        <div className='mt-5 text-center d-grid gap-2'>
-          <Button variant="primary" type="submit" size='lg'>
-            Confirm
-          </Button>
+        <div className='text-center qr-code'>
+          <img src="./qr-code.png" className="img-fluid" alt="..." width="100"></img>
         </div>
-      </section>
-    </Form>
+
+        <section className='main-section'>
+          <Form.Group>
+            <Form.Label>Task</Form.Label>
+            <Form.Select value={task} onChange={onTaskChange}>
+              <option value="1">Member registration</option>
+              <option value="2">ETH Waterloo 2023</option>
+              <option value="3">Collision 2023</option>
+              <option value="4">Blockchain Futurist Conference</option>
+            </Form.Select>
+          </Form.Group>
+
+          <div className="mt-4">
+            {task === "1" && (
+              <>
+                <p>Event: Pragma Waterloo</p>
+                <p>When: June 22, 2023</p>
+                <p>Location: CIGI Campus (Waterloo)</p>
+              </>
+            )}
+            {task === "2" && (
+              <>
+                <p>Event: ETH Waterloo</p>
+                <p>When: June 23, 2023</p>
+                <p>Location: CIGI Campus (Waterloo)</p>
+              </>
+            )}
+          </div>
+
+          <div className='mt-5 text-center d-grid gap-2'>
+            <Button variant="primary" type="submit" size='lg'>
+              Confirm
+            </Button>
+          </div>
+        </section>
+      </Form>
+    </>
   );
 }
 
