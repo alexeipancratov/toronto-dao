2b7d1e757e4f3cfd41a50eddf3e8e8f591ceddb4
Alexei Pancratov <drivergear@gmail.com>
2023-06-24 10:08:56 -0400

    Initial commit - UI

---
+++ ui/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
