dbb241619227f83b0a1b0396711f5e7a25415353
Alexei Pancratov <drivergear@gmail.com>
2023-06-24 17:37:15 -0400

    Implemented member registration

--- ui/src/task-verification/TaskVerification.tsx
+++ ui/src/task-verification/TaskVerification.tsx
@@ -1,11 +1,41 @@
 import { Button, Form } from "react-bootstrap";
+import { ethers } from "ethers";
+import { useMetaMask } from "metamask-react";
+import { useState } from "react";
+import contractAbi from "../abis/abi.json";
 
 function TaskVerification() {
+  const [ethAddress, setEthAddress] = useState("");
+  const { ethereum } = useMetaMask();
+
+  const sendTransaction = async () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(process.env.REACT_APP_CONTRACT_ADDRESS || "", contractAbi, provider);
+    const contractWithSigner = contract.connect(signer);
+
+    const tx = await contractWithSigner.createMember(ethAddress);
+    console.log(tx);
+
+    alert("Transaction sent!");
+  };
+
+  const onFormSubmit = (e: any) => {
+    e.preventDefault();
+
+    sendTransaction();
+  };
+
+  const onAddressChange = (e: any) => {
+    setEthAddress(e.target.value);
+  }
+
   return (
-    <Form>
+    <Form onSubmit={onFormSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>ETH Address</Form.Label>
-        <Form.Control type="email" placeholder="0x000000000000000000" />
+        <Form.Control type="text" placeholder="0x000000000000000000" value={ethAddress} onChange={onAddressChange} />
       </Form.Group>
 
       <div className='text-center qr-code'>
@@ -22,7 +52,7 @@ function TaskVerification() {
         </Form.Group>
 
         <div className='mt-5 text-center d-grid gap-2'>
-          <Button variant="primary" size='lg'>
+          <Button variant="primary" type="submit" size='lg'>
             Confirm
           </Button>
         </div>
